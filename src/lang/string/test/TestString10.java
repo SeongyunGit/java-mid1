304824985e84ab390ef77d4f3a7e6a395a4edf96
Seongyun <okks3579@gmail.com>
2024-10-29 01:50:01 +0900

    2024-10-29

---
+++ src/lang/string/test/TestString10.java
@@ -0,0 +1,14 @@
+package lang.string.test;
+
+public class TestString10 {
+
+    public static void main(String[] args) {
+        String fruits = "apple,banana,mango";
+        String[] parts = fruits.split(",");
+        for (String fruit : parts) {
+            System.out.println(fruit);
+        }
+        String joinedString = String.join("->",parts);
+        System.out.println("joinedString = " + joinedString);
+    }
+}
